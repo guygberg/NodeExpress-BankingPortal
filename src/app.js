f4da007316b1a0683a0ffb8f42557e86a5d43b6c
Guy <guy.gberg@gmail.com>
2020-06-07 13:04:46 +0300

    Completed module2

--- src/app.js
+++ src/app.js
@@ -5,10 +5,34 @@ const app  = express();
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
+const accountData = fs.readFileSync('./src/json/accounts.json','utf8');
+const accounts =JSON.parse(accountData)
+const userData = fs.readFileSync('./src/json/users.json','utf8');
+const users =JSON.parse(userData)
 
 app.get('/',function(req,res){
 
-    res.render("index", {title:'Index'});
+    res.render("index", {title:'Account Summary',accounts: accounts});
+   });
+
+app.get('/savings',function(req,res){
+
+    res.render("account", {account: accounts.savings});
+   });
+
+app.get('/checking',function(req,res){
+
+    res.render("account", {account: accounts.checking});
+   });
+
+app.get('/credit',function(req,res){
+
+    res.render("account", {account: accounts.credit});
+   });
+
+   app.get('/profile',function(req,res){
+
+    res.render("profile", {user: users[0]});
    });
 
    app.listen(3000,()=>{
